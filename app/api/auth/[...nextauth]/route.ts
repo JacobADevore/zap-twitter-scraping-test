d5f3ddf9c1756c6899781afae048a9c18d738c92
Jacob Devore <jacob@blient.com>
2024-06-11 17:17:03 -0400

    completed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
-import { db } from "../../../db";
+import { PlatformType, db } from "../../../db";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(db),
@@ -10,6 +10,23 @@ const handler = NextAuth({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
       version: "2.0", // opt-in to Twitter OAuth 2.0
+      authorization: {
+        params: {
+          scope: "tweet.read tweet.write users.read",
+        },
+      },
+      profile(profile) {
+        return {
+          id: profile.data.id,
+          username: "<X>" + profile.data.username.toLowerCase(),
+          ui_username: "<X>" + profile.data.username,
+          name: profile.data.name ?? profile.data.username,
+          email: null,
+          image: profile.data.profile_image_url ?? "",
+          created_at: new Date().toISOString(),
+          platform: PlatformType.X,
+        };
+      },
     }),
   ],
 });
